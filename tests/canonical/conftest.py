4aff0878ab43bf16a2197024d12fa835916ecc29
EkaterinaMangileva <mangileva.ek@gmail.com>
2024-07-17 13:04:45 +0500

    Пробные настройки для запуска

--- tests/canonical/conftest.py
+++ tests/canonical/conftest.py
@@ -4,4 +4,4 @@ from config import bot, chat_id
 
 @pytest.hookimpl(trylast=True)
 def pytest_sessionfinish(session, exitstatus):
-    bot.send_message(chat_id, "Все формы отправил, отчет будет в ссылке")
+    bot.send_message(chat_id, "Тест по канониклам прошел, а результат тут -")
