fd1d9862e79b629c85cdf581e9737a05f9e3ee2a
EkaterinaMangileva <mangileva.ek@gmail.com>
2024-07-17 18:07:22 +0500

    Пробные настройки для запуска

--- conftest.py
+++ conftest.py
@@ -16,7 +16,7 @@ def driver():
         "credentials_enable_service": False,
         "profile.password_manager_enabled": False
     }
-    chrome_options.add_experimental_option("prefs", prefs)
+    # chrome_options.add_experimental_option("prefs", prefs)
     if os.getenv("HEADLESS") == "False":
         chrome_options.add_argument("--headless")
     driver = webdriver.Chrome(options=chrome_options)
