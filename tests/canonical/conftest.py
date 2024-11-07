76a8b3e230786683f16a92b8a870782b132bd4f8
101internettests <160827243+101internettests@users.noreply.github.com>
2024-11-08 00:12:10 +0500

    Delete tests/canonical/conftest.py

--- tests/canonical/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from config import bot, chat_id
-
-
-@pytest.hookimpl(trylast=True)
-def pytest_sessionfinish(session, exitstatus):
-    bot.send_message(chat_id, "Тест по канониклам прошел, а результат тут - https://101internettests.github.io/megatest/")
