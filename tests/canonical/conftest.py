ebf02fbb585b639de21906707cee3efbf2678146
101internettests <160827243+101internettests@users.noreply.github.com>
2024-11-15 01:35:56 +0500

    Create conftest.py

---
+++ tests/canonical/conftest.py
@@ -0,0 +1,7 @@
+import pytest
+from config import bot, chat_id
+
+
+@pytest.hookimpl(trylast=True)
+def pytest_sessionfinish(session, exitstatus):
+    bot.send_message(chat_id, "Тест по канониклам прошел, а результат тут - https://101internettests.github.io/megatest/")
