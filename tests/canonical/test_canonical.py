3a68920db0b871a969904402faae141abb717347
EkaterinaMangileva <mangileva.ek@gmail.com>
2024-12-13 00:00:09 +0500

    правки для канониклов

--- tests/canonical/test_canonical.py
+++ tests/canonical/test_canonical.py
@@ -31,9 +31,7 @@ class TestCononicals:
 
     @allure.title("Проверка наличия канониклов на странице адреса, 101, Москва")
     def test_101_moskva_address(self, driver):
-        urls = ['https://101internet.ru/moskva/dom/ul-zelyonaya-d-35-id4614611',
-                'https://101internet.ru/moskva/dom/ul-zelyonaya-d-35-id4614611/2',
-                ]
+        urls = ['https://101internet.ru/moskva/dom/ul-zelyonaya-d-35-id4614611']
         for url in urls:
             check = CanonicalPage(driver, url)
             check.open()
@@ -41,8 +39,7 @@ class TestCononicals:
 
     @allure.title("Проверка наличия канониклов на странице второго адреса, 101, Москва")
     def test_101_moskva_address_second(self, driver):
-        urls = ['https://101internet.ru/moskva/dom/ul-sharikopodshipnikovskaya-d-11-id2801124',
-                'https://101internet.ru/moskva/dom/ul-sharikopodshipnikovskaya-d-11-id2801124/2',
+        urls = ['https://101internet.ru/moskva/dom/ul-sharikopodshipnikovskaya-d-11-id2801124'
                 ]
         for url in urls:
             check = CanonicalPage(driver, url)
@@ -73,10 +70,9 @@ class TestCononicals:
             check.open()
             check.check_page_canonicals_providers_kaz()
 
-    @allure.title("Проверка наличия канониклов на страницах провайдеров, 101, Казань")
+    @allure.title("Проверка наличия канониклов на страницах провайдеров, 101, Екатеринбург")
     def test_101_ekb_address(self, driver):
-        urls = ['https://101internet.ru/ekaterinburg/dom/ul-vainera-d-1-id236224',
-                'https://101internet.ru/ekaterinburg/dom/ul-vainera-d-1-id236224/2'
+        urls = ['https://101internet.ru/ekaterinburg/dom/ul-vainera-d-1-id236224'
                 ]
         for url in urls:
             check = CanonicalPage(driver, url)
@@ -106,8 +102,7 @@ class TestCononicals:
 
     @allure.title("Проверка наличия канониклов на странице адреса, 101, Москва")
     def test_101_msk_address_filter(self, driver):
-        urls = ['https://101internet.ru/moskva/dom/sh-altufevskoe-d-1-id2979647?filter=internetAndTV',
-                'https://101internet.ru/moskva/dom/sh-altufevskoe-d-1-id2979647/2?filter=internetAndTV'
+        urls = ['https://101internet.ru/moskva/dom/sh-altufevskoe-d-1-id2979647?filter=internetAndTV'
                 ]
         for url in urls:
             check = CanonicalPage(driver, url)
@@ -116,8 +111,7 @@ class TestCononicals:
 
     @allure.title("Проверка наличия канониклов на странице адреса, 101, Санкт-Петербург")
     def test_101_spb_address_filter(self, driver):
-        urls = ['https://101internet.ru/sankt-peterburg/dom/pr-kt-engelsa-d-92-id3083354?filter=bezlimitniy-intenet',
-                'https://101internet.ru/sankt-peterburg/dom/pr-kt-engelsa-d-92-id3083354/2?filter=bezlimitniy-intenet'
+        urls = ['https://101internet.ru/sankt-peterburg/dom/pr-kt-engelsa-d-92-id3083354?filter=bezlimitniy-intenet'
                 ]
         for url in urls:
             check = CanonicalPage(driver, url)
@@ -160,128 +154,128 @@ class TestCononicals:
         check.open()
         check.check_page_canonicals_sat_msk()
 
-    @allure.title("Проверка наличия канониклов на главной странице, ПОЛ, Санкт-Петербург")
-    def test_pol_main(self, driver):
-        check = CanonicalPage(driver, "https://piter-online.net/")
-        check.open()
-        check.check_page_canonicals_main_pol()
-
-    @allure.title("Проверка наличия канониклов на странице провайдеров, ПОЛ, Санкт-Петербург")
-    def test_pol_providers(self, driver):
-        urls = ['https://piter-online.net/providers',
-                'https://piter-online.net/providers/2',
-                'https://piter-online.net/providers/3',
-                'https://piter-online.net/providers/4',
-                'https://piter-online.net/providers/5'
-                ]
-        for url in urls:
-            check = CanonicalPage(driver, url)
-            check.open()
-            check.check_page_canonicals_providers_pol()
-
-    @allure.title("Проверка наличия канониклов на странице рейтинга, ПОЛ, Санкт-Петербург")
-    def test_pol_rating(self, driver):
-        check = CanonicalPage(driver, "https://piter-online.net/rating")
-        check.open()
-        check.check_page_canonicals_rating_pol()
-
-    @allure.title("Проверка наличия канониклов на странице тарифов, ПОЛ, Санкт-Петербург")
-    def test_pol_rates(self, driver):
-        urls = ['https://piter-online.net/rates',
-                'https://piter-online.net/rates/2',
-                'https://piter-online.net/rates/3',
-                'https://piter-online.net/rates/4',
-                'https://piter-online.net/rates/5'
-                ]
-        for url in urls:
-            check = CanonicalPage(driver, url)
-            check.open()
-            check.check_page_canonicals_rates_pol()
-
-    @allure.title("Проверка наличия канониклов на странице интернета в офис, ПОЛ, Санкт-Петербург")
-    def test_pol_office(self, driver):
-        check = CanonicalPage(driver, "https://piter-online.net/orders/office")
-        check.open()
-        check.check_page_canonicals_office_pol()
-
-    @allure.title("Проверка наличия канониклов на странице адреса, ПОЛ, Санкт-Петербург")
-    def test_pol_address(self, driver):
-        urls = ['https://piter-online.net/dom/nab-rekifontanki-d-1-id3650379',
-                'https://piter-online.net/dom/nab-rekifontanki-d-1-id3650379/2'
-                ]
-        for url in urls:
-            check = CanonicalPage(driver, url)
-            check.open()
-            check.check_page_canonicals_address_pol()
-
-    @allure.title("Проверка наличия канониклов на странице адреса 2, ПОЛ, Санкт-Петербург")
-    def test_pol_address_second(self, driver):
-        urls = ['https://piter-online.net/dom/nab-admiralteiskogokanala-d-5-id167623',
-                'https://piter-online.net/dom/nab-admiralteiskogokanala-d-5-id167623/2'
-                ]
-        for url in urls:
-            check = CanonicalPage(driver, url)
-            check.open()
-            check.check_page_canonicals_address_second_pol()
-
-    @allure.title("Проверка наличия канониклов на главной странице, ПОЛ, Колпино")
-    def test_pol_kolpino_main(self, driver):
-        check = CanonicalPage(driver, "https://piter-online.net/kolpino")
-        check.open()
-        check.check_page_canonicals_main_kolpino_pol()
-
-    @allure.title("Проверка наличия канониклов на странице адреса 3, ПОЛ, Санкт-Петербург")
-    def test_pol_address_kolpino(self, driver):
-        urls = ['https://piter-online.net/dom/ul-mira-beloostrov-d-10-stra-id3149916?filter=bezlimitniy-intenet',
-                'https://piter-online.net/dom/ul-mira-beloostrov-d-10-stra-id3149916/2?filter=bezlimitniy-intenet'
-                ]
-        for url in urls:
-            check = CanonicalPage(driver, url)
-            check.open()
-            check.check_page_canonicals_address_kolpino_pol()
-
-    @allure.title("Проверка наличия канониклов на странице провайдеров, ПОЛ, Колпино")
-    def test_pol_kolpino_providers(self, driver):
-        check = CanonicalPage(driver, "https://piter-online.net/kolpino/providers")
-        check.open()
-        check.check_page_canonicals_providers_kolpino_pol()
-
-    @allure.title("Проверка наличия канониклов на странице поиска, ПОЛ, Колпино")
-    def test_pol_kolpino_tohome(self, driver):
-        check = CanonicalPage(driver, "https://piter-online.net/kolpino/orders/tohome")
-        check.open()
-        check.check_page_canonicals_tohome_kolpino_pol()
-
-    @allure.title("Проверка наличия канониклов на странице рейтинга, ПОЛ, Колпино")
-    def test_pol_kolpino_rating(self, driver):
-        check = CanonicalPage(driver, "https://piter-online.net/kolpino/rating")
-        check.open()
-        check.check_page_canonicals_rating_kolpino_pol()
-
-    @allure.title("Проверка наличия канониклов на странице тарифов, ПОЛ, Колпино")
-    def test_pol_kolpino_rates(self, driver):
-        check = CanonicalPage(driver, "https://piter-online.net/kolpino/rates")
-        check.open()
-        check.check_page_canonicals_rates_kolpino_pol()
-
-    @allure.title("Проверка наличия канониклов на странице интернета в офис, ПОЛ, Колпино")
-    def test_pol_kolpino_office(self, driver):
-        check = CanonicalPage(driver, "https://piter-online.net/kolpino/orders/office")
-        check.open()
-        check.check_page_canonicals_office_kolpino_pol()
-
-    @allure.title("Проверка наличия канониклов на странице провайдеров, ПОЛ, Ленинградская область")
-    def test_pol_providers_lenoblast(self, driver):
-        urls = ['https://piter-online.net/leningradskaya-oblast/providers',
-                'https://piter-online.net/leningradskaya-oblast/providers/2',
-                'https://piter-online.net/leningradskaya-oblast/providers/3',
-                'https://piter-online.net/leningradskaya-oblast/providers/4',
-                'https://piter-online.net/leningradskaya-oblast/providers/5'
-                ]
-        for url in urls:
-            check = CanonicalPage(driver, url)
-            check.open()
-            check.check_page_canonicals_providers_lenoblast_pol()
+    # @allure.title("Проверка наличия канониклов на главной странице, ПОЛ, Санкт-Петербург")
+    # def test_pol_main(self, driver):
+    #     check = CanonicalPage(driver, "https://piter-online.net/")
+    #     check.open()
+    #     check.check_page_canonicals_main_pol()
+    #
+    # @allure.title("Проверка наличия канониклов на странице провайдеров, ПОЛ, Санкт-Петербург")
+    # def test_pol_providers(self, driver):
+    #     urls = ['https://piter-online.net/providers',
+    #             'https://piter-online.net/providers/2',
+    #             'https://piter-online.net/providers/3',
+    #             'https://piter-online.net/providers/4',
+    #             'https://piter-online.net/providers/5'
+    #             ]
+    #     for url in urls:
+    #         check = CanonicalPage(driver, url)
+    #         check.open()
+    #         check.check_page_canonicals_providers_pol()
+    #
+    # @allure.title("Проверка наличия канониклов на странице рейтинга, ПОЛ, Санкт-Петербург")
+    # def test_pol_rating(self, driver):
+    #     check = CanonicalPage(driver, "https://piter-online.net/rating")
+    #     check.open()
+    #     check.check_page_canonicals_rating_pol()
+    #
+    # @allure.title("Проверка наличия канониклов на странице тарифов, ПОЛ, Санкт-Петербург")
+    # def test_pol_rates(self, driver):
+    #     urls = ['https://piter-online.net/rates',
+    #             'https://piter-online.net/rates/2',
+    #             'https://piter-online.net/rates/3',
+    #             'https://piter-online.net/rates/4',
+    #             'https://piter-online.net/rates/5'
+    #             ]
+    #     for url in urls:
+    #         check = CanonicalPage(driver, url)
+    #         check.open()
+    #         check.check_page_canonicals_rates_pol()
+    #
+    # @allure.title("Проверка наличия канониклов на странице интернета в офис, ПОЛ, Санкт-Петербург")
+    # def test_pol_office(self, driver):
+    #     check = CanonicalPage(driver, "https://piter-online.net/orders/office")
+    #     check.open()
+    #     check.check_page_canonicals_office_pol()
+    #
+    # @allure.title("Проверка наличия канониклов на странице адреса, ПОЛ, Санкт-Петербург")
+    # def test_pol_address(self, driver):
+    #     urls = ['https://piter-online.net/dom/nab-rekifontanki-d-1-id3650379',
+    #             'https://piter-online.net/dom/nab-rekifontanki-d-1-id3650379/2'
+    #             ]
+    #     for url in urls:
+    #         check = CanonicalPage(driver, url)
+    #         check.open()
+    #         check.check_page_canonicals_address_pol()
+    #
+    # @allure.title("Проверка наличия канониклов на странице адреса 2, ПОЛ, Санкт-Петербург")
+    # def test_pol_address_second(self, driver):
+    #     urls = ['https://piter-online.net/dom/nab-admiralteiskogokanala-d-5-id167623',
+    #             'https://piter-online.net/dom/nab-admiralteiskogokanala-d-5-id167623/2'
+    #             ]
+    #     for url in urls:
+    #         check = CanonicalPage(driver, url)
+    #         check.open()
+    #         check.check_page_canonicals_address_second_pol()
+    #
+    # @allure.title("Проверка наличия канониклов на главной странице, ПОЛ, Колпино")
+    # def test_pol_kolpino_main(self, driver):
+    #     check = CanonicalPage(driver, "https://piter-online.net/kolpino")
+    #     check.open()
+    #     check.check_page_canonicals_main_kolpino_pol()
+    #
+    # @allure.title("Проверка наличия канониклов на странице адреса 3, ПОЛ, Санкт-Петербург")
+    # def test_pol_address_kolpino(self, driver):
+    #     urls = ['https://piter-online.net/dom/ul-mira-beloostrov-d-10-stra-id3149916?filter=bezlimitniy-intenet',
+    #             'https://piter-online.net/dom/ul-mira-beloostrov-d-10-stra-id3149916/2?filter=bezlimitniy-intenet'
+    #             ]
+    #     for url in urls:
+    #         check = CanonicalPage(driver, url)
+    #         check.open()
+    #         check.check_page_canonicals_address_kolpino_pol()
+    #
+    # @allure.title("Проверка наличия канониклов на странице провайдеров, ПОЛ, Колпино")
+    # def test_pol_kolpino_providers(self, driver):
+    #     check = CanonicalPage(driver, "https://piter-online.net/kolpino/providers")
+    #     check.open()
+    #     check.check_page_canonicals_providers_kolpino_pol()
+    #
+    # @allure.title("Проверка наличия канониклов на странице поиска, ПОЛ, Колпино")
+    # def test_pol_kolpino_tohome(self, driver):
+    #     check = CanonicalPage(driver, "https://piter-online.net/kolpino/orders/tohome")
+    #     check.open()
+    #     check.check_page_canonicals_tohome_kolpino_pol()
+    #
+    # @allure.title("Проверка наличия канониклов на странице рейтинга, ПОЛ, Колпино")
+    # def test_pol_kolpino_rating(self, driver):
+    #     check = CanonicalPage(driver, "https://piter-online.net/kolpino/rating")
+    #     check.open()
+    #     check.check_page_canonicals_rating_kolpino_pol()
+    #
+    # @allure.title("Проверка наличия канониклов на странице тарифов, ПОЛ, Колпино")
+    # def test_pol_kolpino_rates(self, driver):
+    #     check = CanonicalPage(driver, "https://piter-online.net/kolpino/rates")
+    #     check.open()
+    #     check.check_page_canonicals_rates_kolpino_pol()
+    #
+    # @allure.title("Проверка наличия канониклов на странице интернета в офис, ПОЛ, Колпино")
+    # def test_pol_kolpino_office(self, driver):
+    #     check = CanonicalPage(driver, "https://piter-online.net/kolpino/orders/office")
+    #     check.open()
+    #     check.check_page_canonicals_office_kolpino_pol()
+    #
+    # @allure.title("Проверка наличия канониклов на странице провайдеров, ПОЛ, Ленинградская область")
+    # def test_pol_providers_lenoblast(self, driver):
+    #     urls = ['https://piter-online.net/leningradskaya-oblast/providers',
+    #             'https://piter-online.net/leningradskaya-oblast/providers/2',
+    #             'https://piter-online.net/leningradskaya-oblast/providers/3',
+    #             'https://piter-online.net/leningradskaya-oblast/providers/4',
+    #             'https://piter-online.net/leningradskaya-oblast/providers/5'
+    #             ]
+    #     for url in urls:
+    #         check = CanonicalPage(driver, url)
+    #         check.open()
+    #         check.check_page_canonicals_providers_lenoblast_pol()
 
     @allure.title("Проверка наличия канониклов на главной странице, МОЛ, Москва")
     def test_mol_main(self, driver):
@@ -353,8 +347,7 @@ class TestCononicals:
 
     @allure.title("Проверка наличия канониклов на странице адреса, МОЛ, Москва")
     def test_pol_address_mol(self, driver):
-        urls = ['https://www.moskvaonline.ru/dom/ul-arbat-d-1-id218520',
-                'https://www.moskvaonline.ru/dom/ul-arbat-d-1-id218520/2'
+        urls = ['https://www.moskvaonline.ru/dom/ul-arbat-d-1-id218520'
                 ]
         for url in urls:
             check = CanonicalPage(driver, url)
@@ -363,8 +356,7 @@ class TestCononicals:
 
     @allure.title("Проверка наличия канониклов на странице адреса 2, МОЛ, Москва")
     def test_pol_address_second_mol(self, driver):
-        urls = ['https://www.moskvaonline.ru/dom/ul-noviiarbat-d-2-id192977',
-                'https://www.moskvaonline.ru/dom/ul-noviiarbat-d-2-id192977/2'
+        urls = ['https://www.moskvaonline.ru/dom/ul-noviiarbat-d-2-id192977'
                 ]
         for url in urls:
             check = CanonicalPage(driver, url)
