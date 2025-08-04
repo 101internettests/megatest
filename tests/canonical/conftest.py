f996d6d88d369050de48b1a79ef3c787678b5d36
EkaterinaMangileva <mangileva.ek@gmail.com>
2025-08-04 12:36:14 +0500

    пробные изменения для теста

--- tests/canonical/conftest.py
+++ tests/canonical/conftest.py
@@ -4,4 +4,4 @@ from config import bot, chat_id
 
 @pytest.hookimpl(trylast=True)
 def pytest_sessionfinish(session, exitstatus):
-    bot.send_message(chat_id, "Тест по канониклам прошел, а результат тут - https://101internettests.github.io/megatest/")
+    bot.send_message(chat_id, "Тест по канониклам прошел, а результат в дженкинсе")
